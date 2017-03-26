c1e8082067a4567ff1be96aeabba1947939106b5
Ramin <nehramin@gmail.com>
2017-03-25 23:32:04 -0700

    added mapping of data, and some basic output

--- js/toll.js
+++ js/toll.js
@@ -8,30 +8,65 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	request.onload = function() {
 	  if (request.status >= 200 && request.status < 400) {
-	    // Success!
+	    // We retrieved the data successfully.
 	    data = JSON.parse(request.responseText);
 	    var locations  = parseData(data);
+			console.log(locations);
+			printSigns(locations);
 
-
-	    tollCont.innerHTML = locations;
 	  } else {
-	    // We reached our target server, but it returned an error
+	    // We reached our target server, but it returned an error.
 
 	  }
 	};
 
 	request.onerror = function() {
-	  // There was a connection error of some sort
+	  // There was a connection error of some sort.
 	};
 
 	request.send();
 
-	function parseData(data){
+	function parseData(data) {
 		var keys = {};
-		for(var i = 0; i < data.length; i++) 
-			keys.push(data[i].StartLocationName);
+		for (var i = 0; i < data.length; i++) {
+			if (keys[data[i].StartMilepost] == null) {
+				keys[data[i].StartMilepost] = [];
+			}
+			keys[data[i].StartMilepost].push(data[i]);
+		}
 		return keys;
 	}
+
+	function printSigns(locations) {
+		var container = document.getElementById("toll-container");
+		for (var key in locations) {
+			if (locations.hasOwnProperty(key)) {
+					var obj = locations[key];
+					var address = document.createElement("h3");
+					address.innerText = obj[0].StartLocationName + " - " + 
+													obj[0].TravelDirection + " (identifier: " + obj[0].StartMilepost + ")";
+					container.appendChild(address);
+					for (var i = 0; i < obj.length; i++) {
+						var endingLocation = document.createElement("span");
+						endingLocation.innerText = obj[i].EndLocationName;
+
+						var currentMessage = document.createElement("li");
+						currentMessage.innerText = obj[i].CurrentMessage;
+
+						var currentToll = document.createElement("li");
+						currentToll.innerText = "Current Toll: " + obj[i].CurrentToll;
+
+						container.appendChild(endingLocation);
+						container.appendChild(currentMessage);
+						container.appendChild(currentToll);
+					}
+					var breakline = document.createElement("br");
+					container.appendChild(breakline);
+			}
+		}
+	}
+
+
 
   
 });
